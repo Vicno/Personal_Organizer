a8862c727b90e27b86e6ec809f41762dd629136f
stefano Sossi <tenosossi2012@gmail.com>
2020-06-21 11:48:00 -0400

    adding First basic Unit Test

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,69 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    agendas: [
+      {
+        agendaId: "ANG-0001",
+        name: "Work",
+        description: "My Agenda to manage my workd",
+        startHour: "10:00",
+        endHour: "16:00",
+        appointments: [
+          // scheduledAppointments.filter(app => app.agendaId === this.agendaId)
+          // concat
+          // Iterate to simulate by clonning
+          // recursiveAppointments.filter(app => app.agendaId === this.agendaId)
+        ]
+      },
+      // more agendas
+    ],
+    scheduledAppointments: [
+      {
+        name: "Dentist",
+        description: "I need to go to dentist",
+        date: "06/18/2020",
+        startHour: "10:00",
+        endHour: "11:00",
+        agendaId: "ANG-0001",
+        participants: [
+          "PART-001"
+        ]
+      }
+    ],
+    postponedAppointments: [
+      {
+        name: "Dentist",
+        description: "I need to go to dentist",
+        // date: "06/18/2020"
+        // startHour: "10:00",
+        // endHour: "11:00",
+        // agendaId: "ANG-001"
+      }
+    ],
+    recursiveAppointments: [
+      {
+        name: "Dentist",
+        description: "I need to go to dentist",
+        date: "06/19/2020",
+        endDate: "12/18/2020",
+        frequency: "Day|Week|Month",
+        startHour: "10:00",
+        endHour: "11:00",
+        agendaId: "ANG-0001",
+        participants: [
+          "PART-001"
+        ]
+      }
+    ],
+    participants: [
+      {
+        participantId: "PART-001",
+        name: "Person A",
+        contactNumber: "70710101"
+      }
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
---
+++ tests/unit/scheduling.spec.js
@@ -0,0 +1,55 @@
+import { assert } from "chai";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Scheduling from "@/components/Scheduling.vue";
+import store from "@/store";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+
+describe("Scheduling CRUD", () => {
+  it("Validate data shoul pass if data enter", () => {
+    const wrapper = shallowMount(Scheduling);
+    const isValid = wrapper.vm._validateData();
+    assert.isFalse(isValid);
+  });
+  it("Validate data should pass if data enter", () => {
+    const wrapper = shallowMount(Scheduling);
+    wrapper.vm.$data.name = "test";
+    wrapper.vm.$data.description = "description test";
+    wrapper.vm.$data.end_hour = "12:00";
+    wrapper.vm.$data.begin_hour = "11:00";
+    wrapper.vm.$data.date = "2020-05-10";
+    wrapper.vm.$data.agenda = "ANG-0001";
+    wrapper.vm.$data.participants = {};
+    const isValid = wrapper.vm._validateData();
+    assert.isTrue(isValid);
+  });
+  it("Validate hour data should pass if data enter is within limits", () => {
+    const wrapper = shallowMount(Scheduling);
+    wrapper.vm.$data.end_hour = "12:00";
+    wrapper.vm.$data.begin_hour = "11:00";
+    wrapper.vm.$data.agenda = "ANG-0001";
+    const isValid = wrapper.vm._validateHour();
+    assert.isTrue(isValid);
+  });
+  it("Validate date data should pass if data enter is a possible date", () => {
+    const wrapper = shallowMount(Scheduling);
+    wrapper.vm.$data.date = "2020-05-10";
+    const isValid = wrapper.vm._validateDate();
+    assert.isTrue(isValid);
+  });
+});
+describe("LocalVue", () => {
+    it("using the store directly", () => {
+      const localVue = createLocalVue();
+      localVue.use(VueRouter);
+      localVue.use(Vuex);
+      const router = new VueRouter({ routes: [] });
+  
+      let wrapper = shallowMount(Scheduling, {
+        router,
+        store,
+        localVue
+      });
+      assert.equal(wrapper.vm.$store.state.scheduledAppointments.length, 1);
+    });
+});
