997bd8da6e04a9c27efb9f4251a23354ad1d7af3
Vicno <diegorosazza@gmail.com>
2020-06-22 17:09:34 -0400

    Specifyed CRUD UT scenarios for Agenda

---
+++ tests/unit/agenda.spec.js
@@ -0,0 +1,68 @@
+import { assert } from "chai";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Agenda from "@/components/Agenda.vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+import { mockAgenda } from "./mockAgenda";
+
+describe("Agenda CRUD", () => {
+  let localVue;
+  let store;
+  let router;
+  beforeEach(() => {
+    localVue = createLocalVue();
+    localVue.use(VueRouter);
+    localVue.use(Vuex);
+    router = new VueRouter({ routes: [] });
+    store = new Vuex.Store(mockAgenda);
+  });
+  it("Creating a new agenda should work properly", () => {
+    const wrapper = shallowMount(Agenda, {
+      router,
+      store,
+      localVue
+    }); // wrapper = { new Vue(), html, find, findAll, etc }
+    let expectedInitialLength = 1;
+    //const agendas = wrapper.vm.$data.agendas; // wrapper.vm.$data.groups[0]
+    assert.equal(store.agendas.length, expectedInitialLength);
+
+    expectedInitialLength = 2;
+    wrapper.vm.createNewAgenda();
+    assert.equal(store.agendas.length, expectedInitialLength);
+  });
+
+  it("Updating an agenda ahould work properly", () => {
+    const wrapper = shallowMount(Agenda, {
+      router,
+      store,
+      localVue
+    }); // wrapper = { new Vue(), html, find, findAll, etc }
+    const updateVal = {
+      agendaId: "ANG-001",
+      name: "Cambio1",
+      description: "Prueba Update",
+      startHour: "10:00",
+      endHour: "12:00",
+      appointments: []
+    };
+    assert.isFalse().equal(store.agendas[0], updateVal);
+    //const agendas = wrapper.vm.$data.agendas; // wrapper.vm.$data.groups[0]
+    wrapper.vm.updateAgenda(updateVal);
+    assert.equal(store.agendas[0], updateVal);
+  });
+
+  it("Removing an agenda should work properly", () => {
+    const wrapper = shallowMount(Agenda, {
+      router,
+      store,
+      localVue
+    }); // wrapper = { new Vue(), html, find, findAll, etc }
+    let expectedInitialLength = 1;
+    //const agendas = wrapper.vm.$data.agendas; // wrapper.vm.$data.groups[0]
+    assert.equal(store.agendas.length, expectedInitialLength);
+    const id = "ANG-001";
+    expectedInitialLength = 0;
+    wrapper.vm.deleteAgenda(id);
+    assert.equal(store.agendas.length, expectedInitialLength);
+  });
+});
