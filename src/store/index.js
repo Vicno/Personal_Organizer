ab5a35c9bfd7b9fdd0e225bde86cb0b4f989be30
Vicno <diegorosazza@gmail.com>
2020-06-29 20:18:56 -0400

    Addes Persisted State

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import createPersistedState from "vuex-persistedstate";
 import actions from "./actions";
 import getters from "./getters";
 import mutations from "./mutations";
@@ -8,6 +9,11 @@ import mutations from "./mutations";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      //paths: ["agendas"]
+    })
+  ],
   state: {
     agendaSelected: "Work",
     agendas: [
